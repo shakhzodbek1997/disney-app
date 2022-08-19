18e9818c4f70c93ca550a6cea8bf808d66d4a770
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-08-19 12:50:50 +0200

    firebase Added and connected with Google authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import styled from 'styled-components';
+import { auth, provider } from '../firebase';
 
-const Header = (props) => {
+const Header = (props) => { 
+    const handleAuth = () => {
+        auth.signInWithPopup( provider).then((result) => {
+            console.log(result)
+        }).catch((error) => {
+            alert(error.message)
+        })
+    }
   return (
     <Nav>
         <Logo>
@@ -35,7 +43,7 @@ const Header = (props) => {
             </a>    
         </NavMenu>
 
-        <Login>
+        <Login onClick={handleAuth}> 
             Login
         </Login>
     </Nav>
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase";
+import "firebase/auth"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMY96wpm3x-uQ3MwOj4yUyJZjfVtpVqPU",
+  authDomain: "disney-clone-92812.firebaseapp.com",
+  projectId: "disney-clone-92812",
+  storageBucket: "disney-clone-92812.appspot.com",
+  messagingSenderId: "734795365122",
+  appId: "1:734795365122:web:ed09e23f74a10b2573a01b",
+  measurementId: "G-8EBP0CDR62"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
