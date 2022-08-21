7cca1f543af743b7023e8923e73f3f8a5108f8b3
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-08-21 07:30:46 +0200

    bCKGROUND added on Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,45 @@ import styled from "styled-components";
 
 const Home = (props) => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Container>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+            <h1>Hello</h1>
+        </Container>
     )
 }
+
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after{
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    content: '';
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 export default Home;
