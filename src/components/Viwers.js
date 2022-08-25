b848798a25166a5abd4052485874c680383b5bcf
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-08-25 12:02:08 +0200

    all categories added

--- src/components/Viwers.js
+++ src/components/Viwers.js
@@ -1,49 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const Viwers = (props) => {
+const Viewers = (props) => {
   return (
     <Container>
-        <Wrap>
-            <img src='/images/viewers-disney.png' alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} >
-                <source src='/videos/1564674844-disney.mp4' type='video/mp4'/>   
-            </video>
-        </Wrap>
-
-        
-        <Wrap>
-            <img src='/images/viewers-pixar.png' alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} >
-                <source src='/videos/1564676714-pixar.mp4' type='video/mp4'/>   
-            </video>
-        </Wrap>
-
-        
-        <Wrap>
-            <img src='/images/viewers-marvel.png' alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} >
-                <source src='/videos/1564676115-marvel.mp4' type='video/mp4'/>   
-            </video>
-        </Wrap>
-
-        
-        <Wrap>
-            <img src='/images/viewers-starwars.png' alt='' />
-            <video autoPlay={true} loop={true} playsInline={true} >
-                <source src='/videos/1608229455-star-wars.mp4' type='video/mp4'/>   
-            </video>
-        </Wrap>
-
-        
-        <Wrap>
-            <img src='/images/viewers-national.png' alt='' />
-            <video autoplay={true} loop={true} playsinline={true} >
-                <source src='/videos/1564676296-national-geographic.mp4' type='video/mp4'/>   
-            </video>
-        </Wrap>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564674844-disney.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
+        </video>
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </Wrap>
     </Container>
-  )
+  );
 };
 
 const Container = styled.div`
@@ -53,7 +47,6 @@ const Container = styled.div`
   grid-gap: 25px;
   gap: 25px;
   grid-template-columns: repeat(5, minmax(0, 1fr));
-
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
   }
@@ -62,15 +55,14 @@ const Container = styled.div`
 const Wrap = styled.div`
   padding-top: 56.25%;
   border-radius: 10px;
-  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, 
-                rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
-  
-  img{
+  img {
     inset: 0px;
     display: block;
     height: 100%;
@@ -82,7 +74,7 @@ const Wrap = styled.div`
     z-index: 1;
     top: 0;
   }
-  video{
+  video {
     width: 100%;
     height: 100%;
     position: absolute;
@@ -90,15 +82,15 @@ const Wrap = styled.div`
     opacity: 0;
     z-index: 0;
   }
-
-  &:hover{
-    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, 
-                rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-    transform: scale(1.05);  
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
-    video{
-        opacity: 1;
+    video {
+      opacity: 1;
     }
-}`;
+  }
+`;
 
-export default Viwers;
+export default Viewers;
